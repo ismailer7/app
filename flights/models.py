e5d5914eb633d13288e61576c6a29fe329fb0449
ismail <ismailrzouki7@gmail.com>
2018-12-01 01:48:36 +0000

    Manuplating database using shell

--- flights/models.py
+++ flights/models.py
@@ -6,4 +6,6 @@ class Flight(models.Model):
 	origin = models.CharField(max_length=64)
 	destination = models.CharField(max_length=64)
 	dration = models.IntegerField(default=0)
- 
+
+	def __str__(self):
+		return f"{self.id} - {self.origin} to {self.destination}"
