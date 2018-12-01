52d430869cf6c1079e62f282d599d4a5b5678b25
ismail <ismailrzouki7@gmail.com>
2018-12-01 06:27:16 +0000

    Get all flights from database and pass them to the context in render Function

--- flights/views.py
+++ flights/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Airport, Flight
 
 # Create your views here.
 
 def index(request):
-	return render(request, "flights/index.html")
+	context = {
+		'flights' : Flight.objects.all()
+	}
+	return render(request, 'flights/index.html', context=context)
