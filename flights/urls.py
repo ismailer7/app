662d112fc059df5671496a73b8336d62f3b22603
ismail <ismailrzouki7@gmail.com>
2018-12-02 03:41:37 +0000

    Athentication

--- flights/urls.py
+++ flights/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:flight_id>', views.flight, name='flight'),
+    path('login/', views.login_in, name='login'),
+    path('logout/', views.login_out, name='logout')
 ]
--- flights/views.py
+++ flights/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 from .models import Flight
 
 # Create your views here.
 
 def index(request):
-	context = {
-		'flights' : Flight.objects.all()
-	}
-	return render(request, 'flights/index.html', context=context)
+	if request.user.is_authenticated:
+		context = {
+			'flights' : Flight.objects.all()
+		}
+		return render(request, 'flights/index.html', context=context)
+	else:
+		return render(request, 'flights/login.html')
 
 def flight(request, flight_id):
 	try:
@@ -21,3 +26,18 @@ def flight(request, flight_id):
 		'passengers' : flight.passengers.all()
 	}
 	return render(request, 'flights/flight.html', context=context)
+
+def login_in(request):
+	username = request.POST['username']
+	password = request.POST['password']
+	user = authenticate(request, username=username, password=password)
+	if user is not None:
+		login(request, user)
+		return HttpResponseRedirect(reverse('index'))
+	else:
+		return render(request, 'flights/login.html', {'message' : 'Invalid sername or/and passwrod'})
+	return render(request, 'flights/login.html')
+
+def login_out(request):
+	logout(request)
+	return render(request, 'flights/login.html', {'message' : 'You Logged Out'})
