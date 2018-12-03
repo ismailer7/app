41a6646c343be84709f3fdfbbebcc8db61138b81
ismail <ismailrzouki7@gmail.com>
2018-12-03 01:12:59 +0000

    Fix register template

--- flights/views.py
+++ flights/views.py
@@ -8,20 +8,26 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def index(request):
+	''' is the default page view of our airline application. '''
 	if request.user.is_authenticated:
+		# if user is already logged in i will display all available flights on home page.
 		context = {
 			'flights' : Flight.objects.all()
 		}
 		return render(request, 'flights/index.html', context=context)
 	else:
+		# else i will display a login page instead of home page
 		return render(request, 'flights/login.html')
 
 def flight(request, flight_id):
+	''' flight is the view that associated to flight.html page with the flight id passed in the url. '''
 	try:
 		flight = Flight.objects.get(pk=flight_id)
+		# get flight of which the primary key is the flight id, if not exist then raise an exception.
 	except Flight.DoesNotExist:
 		raise Http404('Flight does not exist.')
-
+	# if everythong went fine then render flight.html with the content of flight and also the passengers
+	# associated to that flight.
 	context = {
 		'flight' : flight,
 		'passengers' : flight.passengers.all()
@@ -43,23 +49,21 @@ def login_out(request):
 	logout(request)
 	return render(request, 'flights/login.html', {'message' : 'You Logged Out'})
 
-def register_p(request):
-	form = UserCreationForm()
-	return render(request, 'flights/register.html', {'form' : form})
-
 def register(request):
 	if request.method == 'POST':
+		# means that the user is submitting -> the parameters are going into the body.
 		form = UserCreationForm(request.POST)
 		if form.is_valid():
-			form.save()
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password1')
-			user = authenticate(request,username=username, password=password)
-			login(request, user)
-			return HttpResponseRedirect(reverse('index'))
+			form.save() # save the user.
+			username = form.cleaned_data.get('username') # get the user username
+			password = form.cleaned_data.get('password1') # get the user password
+			user = authenticate(request,username=username, password=password) #authenticate the user
+			login(request, user) # log in
+			return HttpResponseRedirect(reverse('index')) # redirect the user to the index page
 		else:
+			form = UserCreationForm()
 			return render(request, 'flights/register.html', {'form' : form})
 	else: # GET method
 		form = UserCreationForm()
-		return render(request, 'flights/register.html', {'form' : form})
+		return render(request, 'flights/register.html', context={ 'form': form })
 	
